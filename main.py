c13e64d0527d8b4df28182f29f14bb0bdf9c13d2
Douglas <80486907+douglasscottjohnston@users.noreply.github.com>
2021-09-05 02:14:49 +0000

    Removed the default jokes and responses and instead just initialized a database on run and made global variables that represent the database values, this fixes a bug where jokes and responses are repeatedly added. Still need to fix the whitelist command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import discord.ext
 from discord.ext import commands
+from discord.ext.commands import Greedy
 import os
 import random
 from replit import db
@@ -11,152 +12,151 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='-',intents=intents)
 
-#Global variables
-bad_jokes = ["your mom", "Your mom", "You're mom","you're mom", "ur mom", "Ur mom", "urmom", "your mum", "Your mum", "You're mum","you're mum", "ur mum", "Ur mum", "urmum", "69", "deez nuts", "dez nuts", "dz nuts", "deeznuts", "deznuts", "dznuts"]
+# for debugging the databases, uncomment to delete all the keys
+# del db["jokes"]
+# del db["responses"]
+# del db["whitelist"]
 
-responses = [
+#Global variables
+if "jokes" not in db.keys():
+  db["jokes"] = ["your mom", "Your mom", "You're mom","you're mom", "ur mom", "Ur mom", "urmom", "your mum", "Your mum", "You're mum","you're mum", "ur mum", "Ur mum", "urmum", "69", "deez nuts", "dez nuts", "dz nuts", "deeznuts", "deznuts", "dznuts"]
+
+if "responses" not in db.keys():
+  db["responses"] = [
   "This article might help you: https://www.wikihow.com/Write-a-Good-Joke",
   "Your career in comedy is looking bleak",
   "He'll be here all week folks"
-]
+  ]
+
+if "whitelist" not in db.keys():
+  db["whitelist"] = []
+  print(db["whitelist"])
 
 if "responding" not in db.keys():
   db["responding"] = True
   print("Bot is responding")
 
+jokes = db["jokes"].value
+responses = db["responses"].value
+wlist = db["whitelist"].value
+responding = db["responding"]
+
+
+
 #Commands
 @bot.command()
 async def addJoke(ctx, message):
   print("command caught")
-  if "jokes" in db.keys():
-    jokes = db["jokes"]
-    if message in jokes:
-      await ctx.send(message + " is already in the list of bad jokes")
-      return
-    else:
-      jokes.append(message)
-      db["jokes"] = jokes
-      print('The bad joke \"' + message + '\" has been added to the list of bad jokes')
-    await ctx.send('The bad joke \"' + message + '\" has been added to the list of bad jokes')
-
+  if message in jokes:
+    await ctx.send(message + " is already in the list of bad jokes")
+    return
   else:
-    db["jokes"] = message
+    jokes.append(message)
+    db["jokes"].value = jokes
     print('The bad joke \"' + message + '\" has been added to the list of bad jokes')
     await ctx.send('The bad joke \"' + message + '\" has been added to the list of bad jokes')
+  #debug
   print(db.keys())
   print(db["jokes"])
 
 @bot.command()
 async def addResponse(ctx, message):
-  if "responses" in db.keys():
-    respond = db["responses"]
-    if message in respond:
-      await ctx.send(message + " is already in the list of responses")
-      return
-    else:
-      respond.append(message)
-    print('The resonse \"' + message + '\" has been added to the list of responses')
-    await ctx.send('The resonse \"' + message + '\" has been added to the list of responses')
-
+  if message in responses:
+    await ctx.send(message + " is already in the list of responses")
+    return
   else:
-    db["responses"] = message
+    responses.append(message)
+    db["responses"].value = responses
     print('The resonse \"' + message + '\" has been added to the list of responses')
     await ctx.send('The resonse \"' + message + '\" has been added to the list of responses')
+  #debug
   print(db.keys())
   print(db["responses"])
 
 @bot.command()
 async def clearJokes(ctx):
-  db["jokes"].clear()
+  db["jokes"].value.clear()
   print(db["jokes"])
   await ctx.send("Jokes cleared from memory")
 
 @bot.command()
 async def clearResponses(ctx):
-  db["responses"].clear()
+  db["responses"].value.clear()
   print(db["responses"])
   await ctx.send("Responses cleared from memory")
 
 @bot.command()
 async def removeJoke(ctx, message):
-  temp = db["jokes"]
-  i = 0
-  while i < len(temp):
-    if(temp[i] == message):
-      del temp[i]
-      db["jokes"] = temp
-    i += 1
+  for i in range(len(jokes)):
+    if message == jokes[i]:
+      del jokes[i]
+  db["jokes"].value = jokes
+  #debug
+  print(jokes)
   print(db["jokes"])
+
   await ctx.send(message + ' was removed from the list of bad jokes')
 
 @bot.command()
 async def removeResponse(ctx, message):
-  temp = db["responses"]
-  i = 0
-  while i < len(temp):
-    if(temp[i] == message):
-      del temp[i]
-      db["responses"] = temp
-  i += 1
+  for i in range(len(responses)):
+    if message == responses[i]:
+      del responses[i]
+  db["responses"].value = responses
+  #debug
+  print(responses)
   print(db["responses"])
+
   await ctx.send(message + ' was removed from the list of bad jokes')
 
 @bot.command()
 async def listJokes(ctx):
-  await ctx.send("Here's all the bad jokes in memory:\n{}".format(db["jokes"]))
+  await ctx.send("Here's all the bad jokes in memory:\n{}".format(jokes))
 
 @bot.command()
 async def listResponses(ctx):
-  await ctx.send("Here's all the responses in memory:\n{}".format(db["responses"]))
+  await ctx.send("Here's all the responses in memory:\n{}".format(responses))
 
 @bot.command()
 async def responding(ctx, args):
   print("command detected")
   if args:
-    db["responding"] = True
+    responding = True
+    db["responding"] = responding
     await ctx.send("Responding is on")
   else:
-    db["responding"] = False
+    responding = False
+    db["responding"] = responding
     await ctx.send("Responding is off")
 
 @bot.command()
-async def whitelist(ctx, *args):
-  if not args:
-    if "whitelist" in db.keys():
-      await ctx.send(db["whitelist"])
-    else:
-      await ctx.send("A whitelist has not been created")
+async def whitelist(ctx, *members: commands.Greedy[discord.Member]):
+  if not members:
+    print(wlist)
+    await ctx.send(wlist)
   else:
-    if "whitelist" in db.keys():
-      wlist = db["whitelist"]
-      if args in wlist:
-        print(db["whitelist"])
-        await ctx.send(args + " is already on the whitelist")
-      else:
-        wlist.append(args)
-        print(db["whitelist"])
-        await ctx.send(args + " is now on the whitelist")
+    if any(x.name in members for x in wlist):
+      print(wlist)
+      await ctx.send("{} is already on the whitelist".format(ctx.mentions[0]))
     else:
-      db["whitelist"] = args
+      wlist.append(n.name for n in members)
+      db["whitelist"].value = wlist
       print(db["whitelist"])
-      await ctx.send(args + " is now on the whitelist")
+      await ctx.send("{} now on the whitelist".format(members))
 
 @bot.command()
 async def unwhitelist(ctx, args):
-  if "whitelist" in db.keys():
-    wlist = db["whitelist"]
     if args in wlist:
-      for i in range(len(wlist)):
-        if(wlist[i] == args):
-          del wlist[i]
-      db["whitelist"] = wlist
+      wlist.remove(args)
+      db["whitelist"].value = wlist
+      #debug
+      print(wlist)
       print(db["whitelist"])
-      await ctx.send(args + " was removed from the whitelist")
+      await ctx.send("{} was removed from the whitelist".format(args))
     else:
       print(db["whitelist"])
-      await ctx.send(args + " is not on the whitelist")
-  else:
-    await ctx.send("A whitelist has not been created")
+      print("{} is not on the whitelist".format(args))
+      await ctx.send("{} is not on the whitelist".format(args))
 
 #Events
 @bot.event
@@ -177,31 +177,15 @@ async def on_message(Message):
   #   print("User whitelisted, ignoring message")
   #   return
 
-  if db["responding"]:
-    respond = responses
-    joke = bad_jokes
-
-    if "responses" in db.keys() and len(db["responses"]) > 0:
-      for r in db["responses"]:
-        if r not in respond:
-          respond.append(r)
-      print(respond)
-      print("Added cached responses")
-
-    if "jokes" in db.keys() and len(db["jokes"]) > 0:
-      for j in db["jokes"]:
-        if j not in respond:
-          joke.append(j)
-      print(joke)
-      print("Added cached jokes")
-
+  if responding:
     print("looking for bad joke")
-    if any(word in Message.content for word in joke):
-      if "whitelsit" in db.keys() and str(Message.author.id) in db["whitelist"]:
-        print("User whitelistded, ignoring message")
-        return
+    if str(Message.author.id) in wlist:
+        print("User whitelisted, ignoring message")
+    elif any(word in Message.content for word in jokes):
       print("BAD JOKE DETECTED!")
-      await Message.channel.send(Message.author.mention + " BAD JOKE DETECTED!\n{}".format(random.choice(respond)))
+      await Message.channel.send(Message.author.mention + " BAD JOKE DETECTED!\n{}".format(random.choice(responses)))
+    else:
+      print("No bad joke detected")
 
   
   
